e310ea423b813fcbb32a9a2dc84f08683cbdb86b
olabadmus3 <badmussulaimon3@gmail.com>
2025-05-09 13:33:18 +0100

    updated the models

--- userapis/models.py
+++ userapis/models.py
@@ -13,7 +13,7 @@ class Profile(models.Model):
     phone = models.CharField(max_length=15,null=True,blank=True)
     gender = models.CharField(max_length=6,choices=GENDER_CHOICES)
     image = models.ImageField(upload_to='profile',null=True,blank=True,default="https://www.silcharmunicipality.in/wp-content/uploads/2021/02/male-face.jpg")
-    created = models.DateTimeField(auto_now_add=False)
+    created = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.fullname
--- users/settings.py
+++ users/settings.py
@@ -112,6 +112,14 @@ STATIC_URL = 'static/'
 
 MEDIA_URL = '/image/'
 MEDIAL_ROOT = os.path.join(BASE_DIR,'image')
+
+# email configs
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = str(os.getenv('EMAIL_USER'))
+EMAIL_HOST_PASSWORD = str(os.getenv('EMAIL_PASSWORD'))
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
