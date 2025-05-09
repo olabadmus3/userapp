8f1b199acc56c05f27dc4ccf257d8f32d70f98f8
olabadmus3 <badmussulaimon3@gmail.com>
2025-05-09 13:05:59 +0100

    api update

--- userapis/models.py
+++ userapis/models.py
@@ -1,3 +1,21 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+
+GENDER_CHOICES=(
+    ('M','Male')
+    ('F','Female')
+)
+class Profile(models.Model):
+    user = models.OneToOneField(user, on_delete=models.CASCADE)
+    fullname = models.CharField(max_length=255)
+    phone = models.CharField(max_length=15,null=True,blank=True)
+    gender = models.CharField(max_length=6,choices=GENDER_CHOICES)
+    image = models.ImageField(upload_to='profile',null=True,blank=True,default="https://www.silcharmunicipality.in/wp-content/uploads/2021/02/male-face.jpg")
+    created = models.DateTimeField(auto_now_add=False)
+
+    def __str__(self):
+        return self.fullname
+
+
