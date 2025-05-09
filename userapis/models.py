aec3cd3ccfddb45dbe0056400690bd232fed2db7
olabadmus3 <badmussulaimon3@gmail.com>
2025-05-09 13:50:40 +0100

    updated

--- userapis/models.py
+++ userapis/models.py
@@ -4,11 +4,11 @@ from django.contrib.auth.models import User
 
 
 GENDER_CHOICES=(
-    ('M','Male')
+    ('M','Male'),
     ('F','Female')
 )
 class Profile(models.Model):
-    user = models.OneToOneField(user, on_delete=models.CASCADE)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
     fullname = models.CharField(max_length=255)
     phone = models.CharField(max_length=15,null=True,blank=True)
     gender = models.CharField(max_length=6,choices=GENDER_CHOICES)
